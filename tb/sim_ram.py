a7b73bcfaadcf1d0c685e03331c8e74a9b5cf6f8
Thomas Hornschuh <thomas@hornschuh-family.de>
2020-01-11 19:00:55 +0100

    Refactoring tb_loadstore: extract ram simulation to sim_ram.py

---
+++ tb/sim_ram.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+
+from myhdl import *
+
+from rtl.loadstore import *
+
+
+class sim_ram:
+    def __init__(self):
+        self.latency = 1
+
+    def setLatency(self,latency):
+        self.latency=latency
+
+
+    @block
+    def ram_interface(self,ram,bus,clock,reset):
+        """
+            Instantiate a RAM module  for simulation
+
+            ram : RAM Array, should be [Signal(modbv(0)[32:]) for ii in range(0, ram_size)]
+            bus: Data bus (class DbusBundle)
+            clock, reset : Clock and reset 
+        """        
+       
+        wait_states = Signal(intbv(0))  # Signal for simulating wait state logic 
+        adr_reg = Signal(modbv(0)[32:0])
+        write_reg = Signal(modbv(0)[32:0])
+        we_reg = Signal(modbv(0)[4:])
+
+        @always_comb
+        def bus_stall():
+            if self.latency>1:
+                bus.stall_i.next = bus.en_o and wait_states>0
+            else:
+                bus.stall_i.next = False     
+
+
+        @always_seq(clock.posedge,reset=reset)
+        def bus_slave():
+
+            bus.ack_i.next=False
+
+            must_wait = self.latency > 1
+
+            if bus.en_o and must_wait and wait_states==0:
+                wait_states.next = self.latency -1
+                adr_reg.next = bus.adr_o
+                write_reg.next = bus.db_wr
+                we_reg.next = bus.we_o
+                
+            elif ( not must_wait and bus.en_o ) or wait_states != 0:
+
+                if must_wait: 
+                    w = wait_states - 1
+                    wait_states.next = w
+                else:
+                    w = 0    
+
+                if w==0: # When all wait states consumed ack bus cycle
+                    if must_wait:
+                        adr_temp = adr_reg[32:2]
+                        wr_temp = write_reg
+                        we_temp = we_reg
+                    else:
+                        adr_temp = bus.adr_o[32:2]
+                        wr_temp = bus.db_wr
+                        we_temp = bus.we_o
+                    if bus.we_o==0:
+                        bus.db_rd.next = ram[adr_temp]
+                    else:
+                        wd=modbv(0)[32:]
+                        wd[:] = ram[adr_temp]
+                        for i in range(len(we_temp)):         
+                           
+                            if we_temp[i]:
+                                low = i * 8
+                                high = low+8
+                                wd[high:low] = wr_temp[high:low]
+
+                        ram[adr_temp].next = wd 
+
+                    bus.ack_i.next = True    
+
+        return instances()            
--- tb/tb_loadstore.py
+++ tb/tb_loadstore.py
@@ -3,16 +3,15 @@ from __future__ import print_function
 from myhdl import *
 
 from rtl.loadstore import *
-from ClkDriver import *
-
 from rtl.instructions import LoadFunct3, StoreFunct3
 from rtl import config
+
+from sim_ram import * 
+from ClkDriver import *
 
 ram_size=256
 
 store_words= (0xdeadbeef,0x55aaeeff,0x12345678,0x705A8000,0xaabbccdd)
-
-ram_latency = 1 
 
 
 
@@ -36,70 +35,11 @@ def tb(config=config.BonfireConfig(),test_conversion=False):
 
 
     ram = [Signal(modbv(0)[32:]) for ii in range(0, ram_size)]
+    mem = sim_ram()
+    mem_i = mem.ram_interface(ram,bus,clock,reset)
 
     cnt = Signal(intbv(0))
 
-    # Signal for simulating wait state logic 
-    wait_states = Signal(intbv(0))
-    adr_reg = Signal(modbv(0)[32:0])
-    write_reg = Signal(modbv(0)[32:0])
-    we_reg = Signal(modbv(0)[4:])
-   
-    @always_comb
-    def bus_stall():
-        if ram_latency>1:
-            bus.stall_i.next = bus.en_o and wait_states>0
-        else:
-            bus.stall_i.next = False     
-
-    @always_seq(clock.posedge,reset=reset)
-    def bus_slave():
-
-        bus.ack_i.next=False
-
-        must_wait = ram_latency > 1
-
-        if bus.en_o and must_wait and wait_states==0:
-            wait_states.next = ram_latency-1
-            adr_reg.next = bus.adr_o
-            write_reg.next = bus.db_wr
-            we_reg.next = bus.we_o
-            
-        elif ( not must_wait and bus.en_o ) or wait_states != 0:
-
-            if must_wait: 
-                w = wait_states - 1
-                wait_states.next = w
-            else:
-                w = 0    
-
-            if w==0: # When all wait states consumed ack bus cycle
-                if must_wait:
-                    adr_temp = adr_reg[32:2]
-                    wr_temp = write_reg
-                    we_temp = we_reg
-                else:
-                    adr_temp = bus.adr_o[32:2]
-                    wr_temp = bus.db_wr
-                    we_temp = bus.we_o
-                if bus.we_o==0:
-                    bus.db_rd.next = ram[adr_temp]
-                else:
-                    wd=modbv(0)[32:]
-                    wd[:] = ram[adr_temp]
-                    for i in range(len(we_temp)):         
-                       
-                        if we_temp[i]:
-                            low = i * 8
-                            high = low+8
-                            wd[high:low] = wr_temp[high:low]
-
-                    ram[adr_temp].next = wd 
-
-                bus.ack_i.next = True    
-           
-          
-    
     fetch_index = Signal(intbv(0))
     
     def sw_test():
@@ -314,13 +254,13 @@ def tb(config=config.BonfireConfig(),test_conversion=False):
     
     @instance
     def stimulus():
-       global ram_latency
+      
 
-       ram_latency=1 
+       mem.setLatency(1) 
        yield run_all()
        yield clear_ram()
        print("Run with RAM wait state")
-       ram_latency=2
+       mem.setLatency(2) 
        yield run_all()
        raise StopSimulation
          
