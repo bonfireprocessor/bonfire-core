7b9d37b6dfe3d59f378d2343361c37122f94fd88
Thomas Hornschuh <thomas@hornschuh-family.de>
2020-08-24 02:02:39 +0200

    tb_cache.py fixes

--- tb/tb_cache.py
+++ tb/tb_cache.py
@@ -25,6 +25,8 @@ def tb(test_conversion=False):
     adr = Signal(modbv(0)[conf.line_select_adr_bits:])
     
     t_r_i = tag_ram_instance(t_in,t_out,we,adr,clock,reset,conf)
+    if test_conversion:
+        t_r_i.convert(hdl='VHDL',std_logic_ports=True,path='vhdl_gen', name="tb_cache")
 
 
     @instance
@@ -36,17 +38,23 @@ def tb(test_conversion=False):
         we.next = True
         for i in range(0,16):
             adr.next = i  
-            t_in.address = i
-            t_in.valid = True
-            t_in.dirty = False
+            t_in.address.next = i
+            t_in.valid.next = True
+            t_in.dirty.next = False
 
             yield clock.posedge 
 
+
+        fs = "address: {address}, valid:{valid}, dirty:{dirty} @{adr}"
         we.next = False
+        adr.next = 0
+        yield clock.posedge
+
         for i in range(0,16):
             adr.next = i  
             yield clock.posedge
-            print(t_out.__dict__) 
+            print(fs.format(adr=adr,**t_out.__dict__))   
+            
 
 
         print ("Simulation finished")
