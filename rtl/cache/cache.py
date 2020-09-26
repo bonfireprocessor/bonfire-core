5dca69bb122589d823e87dd9206d2d2c0c292449
Thomas Hornschuh <thomas@hornschuh-family.de>
2020-09-26 20:15:51 +0200

    Refactoring anc encapsulion of bus interface completed

--- rtl/cache/cache.py
+++ rtl/cache/cache.py
@@ -12,6 +12,7 @@ from rtl.util import int_log2
 import rtl.cache.cache_way as cache_way
 from  rtl.cache.config import CacheConfig
 from rtl.cache.cache_ram import CacheRAMBundle,cache_ram_instance
+from rtl.cache.cache_businterface import BusOutputBundle, BusInputBundle, cache_dbslave_connect
 from rtl.bonfire_interfaces import Wishbone_master_bundle
 
 class CacheControlBundle:
@@ -29,75 +30,6 @@ class CacheMasterWishboneBundle(Wishbone_master_bundle):
         b4_pipelined=False,
         bte_signals=True)
 
-class BusInputBundle():
-    def __init__(self,config,xlen=32):
-        self.slave_en = Signal(bool(0))
-        self.slave_adr_slice = Signal(modbv(0)[config.address_bits:]) # Slave adress slice stripped from lower (not used...) bits
-        self.slave_we = Signal(modbv(0)[xlen//8:])
-        self.slave_write = Signal(modbv(0)[xlen:])
-
-class BusOutputBundle():
-    def __init__(self,config,xlen=32):        
-        self.slave_read =  Signal(modbv(0)[xlen:])
-      
-
-@block
-def cache_dbslave_connect(db_slave,bus_input,bus_output,hit,clock,reset,config):
-
-     # Constants
-    slave_adr_low = db_slave.adrLow  
-    slave_adr_high = slave_adr_low + config.address_bits
-
-    slave_adr_reg =  Signal(modbv(0)[config.address_bits:])
-    en_r = Signal(bool(0)) # Registered slave en signal
-    slave_we_r = Signal(modbv(0)[len(db_slave.we_o):])
-    slave_write_r = Signal(modbv(0)[db_slave.xlen:])
-    slave_ack = Signal(bool(0))
-
-    @always_comb
-    def proc_input_comb():
-
-        bus_input.slave_en.next = en_r or db_slave.en_o
-        db_slave.stall_i.next = en_r and db_slave.en_o
-
-        if en_r:
-            bus_input.slave_adr_slice.next = slave_adr_reg
-            bus_input.slave_we.next = slave_we_r
-            bus_input.slave_write.next = slave_write_r
-        else:
-            bus_input.slave_adr_slice.next = db_slave.adr_o[slave_adr_high:slave_adr_low]
-            bus_input.slave_we.next = db_slave.we_o
-            bus_input.slave_write.next = db_slave.db_wr
-
-    @always_comb
-    def proc_output_comb():
-
-        db_slave.ack_i.next = slave_ack
-        db_slave.db_rd = bus_output.slave_read
-
-
-    # Registers that do not need to be reset
-    @always(clock.posedge)
-    def proc_reg_slave():
-        if db_slave.en_o and not ( en_r or hit ):
-            slave_adr_reg.next = db_slave.adr_o[slave_adr_high:slave_adr_low]
-            slave_we_r.next = db_slave.we_o
-            slave_write_r.next = db_slave.db_wr        
-
-    @always_seq(clock.posedge,reset)
-    def proc_slave_control():
-
-        if db_slave.en_o and not ( en_r or hit ):            
-            en_r.next = True
-            
-
-        if hit and  ( db_slave.en_o or  en_r ):
-            slave_ack.next = True
-            en_r.next = False
-        else:
-           slave_ack.next = False
-
-    return instances()
 
 
 @block
