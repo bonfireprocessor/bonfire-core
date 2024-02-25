950e0bf5f92ba0530312022d3ac05e7214461eda
Thomas Hornschuh <thomas@bonfirecpu.eu>
2024-02-25 21:43:07 +0100

    Support for Laned Memory (not tested)

--- uncore/ram_dp.py
+++ uncore/ram_dp.py
@@ -9,38 +9,60 @@ from __future__ import print_function
 from myhdl import *
 
 
-class RamPort32:
-    def __init__(self,adrWidth=12,readOnly=False):
+class RamPort:
+    def __init__(self,adrWidth=12,dataWidth=32,readOnly=False):
+        assert dataWidth % 8 == 0, "RamPort dataWidth must be multiple of 8"
         self.readOnly = readOnly
         self.clock = Signal(bool(0))
-        self.dbout = Signal(modbv(0)[32:])
+        self.dbout = Signal(modbv(0)[dataWidth:])
         self.adrbus =  Signal(modbv(0)[adrWidth:])
         self.en = Signal(bool(0))
+
         if not readOnly:
-            self.wren=Signal(modbv(0)[4:])
-            self.dbin =  Signal(modbv(0)[32:])
+            wren_length = dataWidth // 8
+            self.wren=Signal(modbv(0)[wren_length:])
+            self.dbin =  Signal(modbv(0)[dataWidth:])
 
 
-def create_ram(ramfile,ramsize):
+# For backwards compatibilty
+class RamPort32(RamPort):
+    def __init__(self,adrWidth=12,readOnly=False):
+        super().__init__(adrWidth=adrWidth,readOnly=readOnly)
+
+
+
+def create_ram(ramfile,ramsize,dataWidth=32):
     ram = []
     adr = 0
 
     f=open(ramfile,"r")
     for line in f:
         i=int(line,16)
-        ram.append(Signal(intbv(i)[32:]))
+        ram.append(Signal(intbv(i)[dataWidth:]))
         adr += 1
 
-    print("eof at adr:{}".format(hex(adr<<2)))    
+    print("eof at adr:{}".format(hex(adr<<2)))
     for i in range(adr,ramsize):
-        ram.append(Signal(intbv(0)[32:]))
+        ram.append(Signal(intbv(0)[dataWidth:]))
 
-    print("Created ram with size {} words".format(len(ram)))
+    print("Created ram with size {} words, width={}".format(len(ram),dataWidth))
     return ram
+
 
 
 @block
 def port_instance(ram,port):
+    """
+
+    Connects a RAM Array to a port.
+    In this ways it creates the actual BRAM instance
+
+    Args:
+        ram (List of intbv): RAM Array
+        port (RamPort): Port to access the RAM Array
+
+
+    """
 
     if port.readOnly:
         @always(port.clock.posedge)
@@ -52,15 +74,16 @@ def port_instance(ram,port):
         @always(port.clock.posedge)
         def ram_proc():
             if port.en:
-                for i in range(len(port.wren)):            
+                for i in range(len(port.wren)):
                     if port.wren[i]:
                         low = i * 8
                         high = low+8
                         ram[port.adrbus].next[high:low] = port.dbin[high:low]
 
                 port.dbout.next = ram[port.adrbus]
-                
+
     return instances()
+
 
 @block
 def dbusToRamPort(dbus,port,clock,readOnly=False):
@@ -79,7 +102,7 @@ def dbusToRamPort(dbus,port,clock,readOnly=False):
         dbus.ack_i.next = ack_rd or ack_wr
         dbus.error_i.next = False
         dbus.stall_i.next = False
-        
+
 
     if not readOnly:
         @always_comb
@@ -102,8 +125,8 @@ class DualportedRam:
     def __init__(self,initFileName, adrwidth=12):
         self.adrwidth=adrwidth
         self.ram = create_ram(initFileName,2**adrwidth)
-       
-        
+
+
     @block
     def ram_instance(self,porta,portb,clock):
         """
@@ -116,13 +139,13 @@ class DualportedRam:
 
         return instances()
 
-
+    # TODO: Check if obsolete !!!!
     @block
     def ram_instance_dbus(self,db_a,db_b,clock):
         """
         db_a: dbus instance, port A
         db_b: dbus instance  port B
-        
+
         """
         porta = RamPort32(self.adrwidth,db_a.readOnly)
         portb = RamPort32(self.adrwidth,db_b.readOnly)
@@ -131,5 +154,85 @@ class DualportedRam:
         p2 = dbusToRamPort(db_b,portb,clock,db_b.readOnly)
 
         ram = self.ram_instance(porta,portb,clock)
+
+        return instances()
+
+
+class DualportedRamLaned:
+
+
+    def create_ram(self,ramfile,ramsize,dataWidth=32):
+
+        self.ram = [ list() for i in range(4) ]
+
+        adr = 0
+
+        f=open(ramfile,"r")
+        for line in f:
+            v=int(line,16)
+            temp=modbv(v)[32:0]
+            self.ram[0].append(Signal(intbv(temp[8:0])[8:]))
+            self.ram[1].append(Signal(intbv(temp[16:8])[8:]))
+            self.ram[2].append(Signal(intbv(temp[24:16])[8:]))
+            self.ram[3].append(Signal(intbv(temp[32:24])[8:]))
+
+            adr += 1
+
+        print("eof at adr:{}".format(hex(adr<<2)))
+        for i in range(adr,ramsize):
+            for k in range(0,4):
+                self.ram[i].append(Signal(intbv(0)[8:]))
+
+
+        print("Created  laned ram with size {} words".format(len(self.ram[0])))
+
+
+
+    def __init__(self,initFileName, adrwidth=12):
+        self.adrwidth=adrwidth
+        self.ram = create_ram(initFileName,2**adrwidth)
+
+
+    @block
+    def ram_instance(self,porta,portb,clock):
+        """
+
+        porta: 32 Bit(!) RamPort instance, port A
+        portb: 32 Bit(!)  RamPort instance  port B
+        """
+        lpa = [None for i in range (4)]
+        lpb = [None for i in range (4)]
+
+        ia = [None for i in range (4)]
+        ib = [None for i in range (4)]
+       
+        for i in range(4):
+            # Create RAM Ports for every lane
+            lpa[i] = RamPort(adrWidth=self.adrwidth,dataWidth=8,readOnly=porta.readOnly)
+            lpb[i] = RamPort(adrWidth=self.adrwidth,dataWidth=8,readOnly=portb.readOnly)
+            # Wire them to the RAM lanes
+            ia[i] = port_instance(self.ram[i],lpa[i])
+            ib[i] = port_instance(self.ram[i],lpb[i])
+
+
+        @always_comb
+        def map_port():
+            for i in range(4):
+                low = i*8
+                high = (i+1)*8
+                lpa[i].clock.next = porta.clock
+                lpa[i].adrbus.next = porta.adrbus
+                lpa[i].en.next = porta.en
+                lpa[i].dbin.next = porta.dbin[high:low]
+                lpa[i].wren.next[0] = porta.wren[i]
+                porta.dbout.next[high:low] = lpa.dbout
+
+                lpb[i].clock.next = portb.clock
+                lpb[i].adrbus.next = portb.adrbus
+                lpb[i].en.next = portb.en
+                lpb[i].dbin.next = portb.dbin[high:low]
+                lpb[i].wren.next[0] = portb.wren[i]
+                portb.dbout.next[high:low] = lpb.dbout
+                
 
         return instances()
